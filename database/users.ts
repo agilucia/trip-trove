e56918b6784a813cee3ae958fbf4f96ea9a018f2
Agnes Pilar <agnes.pilar@yahoo.de>
2023-05-08 10:58:48 +0200

    Create queries for users table

---
+++ database/users.ts
@@ -0,0 +1,86 @@
+import { cache } from 'react';
+import { sql } from './connect';
+
+export type User = {
+  id: number;
+  username: string;
+};
+
+type UserWithPasswordHash = User & {
+  passwordHash: string;
+};
+
+export const getUserBySessionToken = cache(async (token: string) => {
+  const [user] = await sql<{ id: number; username: string }[]>`
+    SELECT
+      users.id,
+      users.username
+    FROM
+      users
+    INNER JOIN
+      sessions ON (
+        sessions.token = ${token} AND
+        sessions.user_id = users.id AND
+        sessions.expiry_timestamp > now()
+      )
+  `;
+  return user;
+});
+
+export const getUserByUsernameWithPasswordHash = cache(
+  async (username: string) => {
+    const [user] = await sql<UserWithPasswordHash[]>`
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      username = ${username}
+  `;
+    return user;
+  },
+);
+
+export const getUserByUsername = cache(async (username: string) => {
+  const [user] = await sql<{ id: number; username: string }[]>`
+    SELECT
+      id,
+      username
+    FROM
+      users
+    WHERE
+      username = ${username}
+  `;
+  return user;
+});
+
+export const createUser = cache(
+  async (username: string, passwordHash: string) => {
+    const [user] = await sql<{ id: number; username: string }[]>`
+      INSERT INTO users
+        (username, password_hash)
+      VALUES
+        (${username}, ${passwordHash})
+      RETURNING
+        id,
+        username
+    `;
+    return user;
+  },
+);
+
+export const getAllUsers = cache(async () => {
+  const users = await sql<UserWithPasswordHash[]>`
+  SELECT * FROM users
+  `;
+
+  return users;
+});
+
+export type UserWithLocationFavorites = {
+  userId: number;
+  userName: string;
+  favoriteId: number | null;
+  locationId: number | null;
+  locationName: string | null;
+};
